4887e2e932346a82bc8894ca64f71e5cbd4f9b43
skadmsfl <noeytt.pk@gmail.com>
2019-10-16 20:02:45 +0700

    TAG

--- app/src/main/java/com/example/test6/MainActivity.kt
+++ app/src/main/java/com/example/test6/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.test6
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+
+    }
+
+    private inner class DownloadData : AsyncTask<String, Void, String>() {
+        private val TAG = "DownloadData"
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 }
