2dc4abf71213e3d3d3cc3e96a152b6a68aac1d3e
skadmsfl <noeytt.pk@gmail.com>
2019-10-16 20:48:35 +0700

    main

--- app/src/main/java/com/example/test6/MainActivity.kt
+++ app/src/main/java/com/example/test6/MainActivity.kt
@@ -74,8 +74,8 @@ class MainActivity : AppCompatActivity() {
                     Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
                 } catch (e: IOException) {
                     Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
-//                } catch (e: Exception) {
-//                    Log.e(TAG, "Unknown error: ${e.message}")
+                } catch (e: Exception) {
+                    Log.e(TAG, "Unknown error: ${e.message}")
                 }
 
                 return ""  // If it gets to here, there's been a problem. Return an empty string
