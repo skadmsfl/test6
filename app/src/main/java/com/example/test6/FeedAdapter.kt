643db7479aac49d8761758b8277bc2b0eca115f4
skadmsfl <noeytt.pk@gmail.com>
2019-10-17 21:04:02 +0700

    override

--- app/src/main/java/com/example/test6/FeedAdapter.kt
+++ app/src/main/java/com/example/test6/FeedAdapter.kt
@@ -18,35 +18,24 @@ class ViewHolder(v: View) {
 class FeedAdapter(context: Context, private val resource: Int, private val applications: List<FeedEntry>)
     : ArrayAdapter<FeedEntry>(context, resource) {
 
-    private val TAG = "FeedAdapter"
+    //    private val TAG = "FeedAdapter"
     private val inflater = LayoutInflater.from(context)
 
-    override fun getCount(): Int {
-        Log.d(TAG, "getCount() called")
-        return applications.size
-    }
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        Log.d(TAG, "getView() called")
+//        Log.d(TAG, "getView called")
         val view: View
         val viewHolder: ViewHolder
 
         if (convertView == null) {
-            Log.d(TAG, "getView called with null convertView")
+//            Log.d(TAG, "getView called with null convertView")
             view = inflater.inflate(resource, parent, false)
             viewHolder = ViewHolder(view)
             view.tag = viewHolder
         } else {
-            Log.d(TAG, "getView provided a convertView")
+//            Log.d(TAG, "getView provided a convertView")
             view = convertView
             viewHolder = view.tag as ViewHolder
         }
-
-
-//        val tvName: TextView = view.findViewById(R.id.tvName)
-//        val tvArtist: TextView = view.findViewById(R.id.tvArtist)
-//        val tvSummary: TextView = view.findViewById(R.id.tvSummary)
 
         val currentApp = applications[position]
 
@@ -55,5 +44,10 @@ class FeedAdapter(context: Context, private val resource: Int, private val appli
         viewHolder.tvSummary.text = currentApp.summary
 
         return view
+    }
+
+    override fun getCount(): Int {
+//        Log.d(TAG, "getCount called")
+        return applications.size
     }
 }
